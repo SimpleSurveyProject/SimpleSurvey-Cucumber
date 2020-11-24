c3ec5e874e704b4e5abd3e64ebddac21f2f640cf
Simon <simon2022@github.com>
2020-11-24 13:14:08 +0100

    cucumber running

---
+++ test/features/ExampleTest.java
@@ -0,0 +1,24 @@
+package features;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ExampleTest {
+
+	@Given("can reach website")
+	public void can_reach_website() {
+//		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user visits site")
+	public void user_visits_site() {
+//		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("hello-world is shown")
+	public void hello_world_is_shown() {
+//		throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ test/features/case.feature
@@ -0,0 +1,8 @@
+Feature: Example
+
+Background:
+ Given can reach website
+
+Scenario: correct content is delivered
+ When user visits site
+ Then hello-world is shown
